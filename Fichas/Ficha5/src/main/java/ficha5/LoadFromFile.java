93ddd88f27fa49412e0910ed62f594524b9180bf
JuanFernandes99 <juandanieldosr@gmail.com>
2022-03-03 17:53:56 +0000

    Ficha 5

---
+++ Fichas/Ficha5/src/main/java/ficha5/LoadFromFile.java
@@ -0,0 +1,26 @@
+package ficha5;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class LoadFromFile {
+
+	public void obterInformacaoFicheiro(String nomeFicheiro) {
+
+		getFileAsIOStream();
+		getFileContent();
+		convertStringToPessoa();
+	}
+
+	public void getFileAsIOStream() {
+
+	}
+
+	public void getFileContent() {
+
+	}
+
+	public void convertStringToPessoa() {
+
+	}
+}
